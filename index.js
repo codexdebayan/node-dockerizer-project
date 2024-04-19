5c25948cc04f2d6022044defa852752ad0f35cd0
DEBAYAN <cdebayan24@gmail.com>
2024-04-20 02:56:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 2000;
+
+const message = "Welcome to my Node.js App!";
+
+// Define a route for the root URL '/'
+app.get('/', (req, res) => {
+    // Read the HTML file
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) {
+            // Error handling
+            res.status(500).send('Internal Server Error');
+        } else {
+            // Set the appropriate content type
+            res.setHeader('Content-Type', 'text/html');
+            // Replace the placeholder with the message
+            const renderedHTML = data.toString().replace('{message}', message);
+            // Send the modified HTML to the client
+            res.send(renderedHTML);
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
