ae23e94d74c82f158ac0c9cb4a8bc5b315799134
DEBAYAN <cdebayan24@gmail.com>
2024-04-22 00:25:32 +0530

    changed port

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:latest
 WORKDIR /apps
 COPY package.json .
 ADD . .
-EXPOSE 2000
+EXPOSE 4200
 RUN npm install
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 2000;
+const port = 4200;
 
 const message = "Welcome to my Node.js App!";
 
